36516890eaf7e2e2a14fa010a902608134a52b37
Aiden Grossman <agrossman154@yahoo.com>
2025-02-26 22:31:45 -0800

    Add support for querying data from the firmware
    This patch adds support for the software to query for data from the
    firmware. Currently this happens once every ten seconds on a separate
    thread.

--- software/software.py
+++ software/software.py
@@ -3,10 +3,34 @@
 The web backend for the software for our final project.
 """
 
+import datetime
+import threading
+
 from flask import Flask
 from flask import send_file
+import requests
+
+FIRMWARE_MOCK_ADDRESS = "http://localhost:8080"
 
 app = Flask(__name__)
+
+time_stamps = []
+ph = []
+temp =[]
+humidity = []
+conductivity = []
+
+def queryData():
+  threading.Timer(10.0, queryData).start()
+
+  time_stamps.append(str(datetime.datetime.now()))
+  req_ph = requests.get(FIRMWARE_MOCK_ADDRESS + "/ph")
+  ph.append(req_ph.json()['ph'])
+  req_temp = requests.get(FIRMWARE_MOCK_ADDRESS + "/temp")
+  temp.append(req_temp.json()['temperature'])
+  humidity.append(req_temp.json()['humidity'])
+  req_conductivity = requests.get(FIRMWARE_MOCK_ADDRESS + "/do")
+  conductivity.append(req_conductivity.json()['do'])
 
 @app.route("/")
 def root():
@@ -15,10 +39,14 @@ def root():
 @app.route("/data")
 def data():
   return {
-    "times": [1,2,3],
-    "temperature": [1,2,3],
-    "humidity": [1,2,3],
-    "ph": [1,2,3],
-    "conductivity": [1,2,3]
+    "times": time_stamps,
+    "temperature": temp,
+    "humidity": humidity,
+    "ph": ph,
+    "conductivity": conductivity
   }
-    
+
+if __name__ == "__main__":
+  queryData()
+  app.run(host="0.0.0.0", port=5000)
+
